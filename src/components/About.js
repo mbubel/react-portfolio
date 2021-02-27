000c1fad66fe162cdbbabfdcb9a505f3df17c980
Michael <michaelbubel.1989@gmail.com>
2021-02-26 17:52:34 -0800

    Edited About Me text

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,23 @@ function About(props) {
     <div>
       <p>
         Born and raised in Rancho San Diego, I spent most of my childhood either
-        playing some sort of video game or skateboarding with my friends. My
-        first discovery of any type of wed developement was fixing up my
-        Myspace. Although it's the most basic of CSS, I always loved doing it.
+        playing some sort of video game or skateboarding with my friends.{" "}
+      </p>
+      My first discovery of any type of wed developement was fixing up my
+      Myspace. Although it's the most basic of CSS, I always loved doing it.
+      <p>
         I've been working in the Shipping/Receiving industry for nearly 14
         years, but I decided mid 2020 that I needed a change. Insert the UCSD
-        Bootcamp Program! My hobbies still include playing video games, but I've
-        retired the skateboarding. When I'm not behind a computer, you can catch
-        me at a local bar or restaurant, or camping with my buddies. Nothing
-        beats sitting around a campfire, beer in hand, and stories to tell. I
-        current reside in Hillcrest, along with my lovely girlfriend, and our
+        Bootcamp Program!
+      </p>
+      <p>
+        My hobbies still include playing video games, but I've retired the
+        skateboarding. When I'm not behind a computer, you can catch me at a
+        local bar or restaurant, or camping with my buddies. Nothing beats
+        sitting around a campfire, beer in hand, and stories to tell.{" "}
+      </p>
+      <p>
+        I current reside in Hillcrest, along with my lovely girlfriend, and our
         menegerie of animals. We're the proud pet parents of three cats: Zero,
         Gambit, and Rogue, as well as a female two year old English Mastiff:
         Bear!
