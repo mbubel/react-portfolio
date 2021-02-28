e15afbb19fb1267300cdff43a1a9154d1e6710a7
Michael <michaelbubel.1989@gmail.com>
2021-02-28 01:29:53 -0800

    changed the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 function Header(props) {
   return (
     <div className="header fixed-top">
-      <Link to="/" className="headerLink" >Home </Link>
-      <Link to="/about" className="headerLink"> About </Link>
+      <Link to="/" className="headerLink" >About Me </Link>
+      <Link to="/about" className="headerLink"> Biography </Link>
       <Link to="/projects" className="headerLink"> Projects</Link>
     </div>
   );
