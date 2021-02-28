3d9bf275430b0cd56ce828cb67c3302a8a6e3e37
Michael <michaelbubel.1989@gmail.com>
2021-02-28 01:26:45 -0800

    fixed complied suggestions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,33 +10,42 @@ function Footer(props) {
       <a
         className="footIcons"
         target="_blank"
+        rel="noreferrer"
         href="https://www.linkedin.com/in/michael-bubel-b95268155/"
       >
-        <img src={linked} height="20vw" alt="linkedin icon"/>
-      </a>
-      <a className="footIcons" target="_blank" href="https://github.com/mbubel">
-        <img src={github} height="20vw" alt="github icon"/>
+        <img src={linked} height="20vw" alt="linkedin icon" />
       </a>
       <a
         className="footIcons"
         target="_blank"
+        rel="noreferrer"
+        href="https://github.com/mbubel"
+      >
+        <img src={github} height="20vw" alt="github icon" />
+      </a>
+      <a
+        className="footIcons"
+        target="_blank"
+        rel="noreferrer"
         href="https://www.facebook.com/heyitsmichaelbubel"
       >
-        <img src={facebook} height="20vw" alt="facebook icon"/>
+        <img src={facebook} height="20vw" alt="facebook icon" />
       </a>
       <a
         className="footIcons"
         target="_blank"
+        rel="noreferrer"
         href="mailto: michaelbubel.1989@gmail.com"
       >
-        <img src={email} height="20vw" alt="email icon"/>
+        <img src={email} height="20vw" alt="email icon" />
       </a>
       <a
         className="footIcons"
         target="_blank"
+        rel="noreferrer"
         href="https://drive.google.com/file/d/1bVn-607BJAn7uCcHLKKJvcf-RCblEkwZ/view?usp=sharing"
       >
-        <img src={resume} height="20vw" alt="resume icon"/>
+        <img src={resume} height="20vw" alt="resume icon" />
       </a>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,51 +8,62 @@ function Projects(props) {
     <div id="projectsGrid" class="container">
       <div class="row-sm">
         <div class=".col-sm-4">
-          <a>
-            <h2>Student registration</h2>
+          <h2>Student registration</h2>
+          <img
+            id="studentRegistration"
+            target="_blank"
+            rel="noreferrer"
+            src={studentReg}
+            className="img-thumbnail img-fluid"
+            alt="student registration"
+          ></img>
+          <a href="https://github.com/mbubel/student-registration">
             <img
-              id="studentRegistration"
               target="_blank"
-              src={studentReg}
-              className="img-thumbnail img-fluid"
-              alt = "student registration picture"
-            ></img>
-            <a href="https://github.com/mbubel/student-registration">
-              <img target="_blank" src={github} className="projectIcons " alt="github icon" />
-            </a>
-            <a href="https://student-registration-tracker.herokuapp.com/">
-              <img target="_blank" src={heroku} className="projectIcons " alt ="heroku icon"/>
-            </a>
+              rel="noreferrer"
+              src={github}
+              className="projectIcons "
+              alt="github icon"
+            />
+          </a>
+          <a href="https://student-registration-tracker.herokuapp.com/">
+            <img
+              target="_blank"
+              src={heroku}
+              className="projectIcons "
+              alt="heroku icon"
+            />
           </a>
         </div>
       </div>
       <div class="row-sm">
         <div class=".col-sm-4">
-          <a>
-            <h2>Fitness Tracker</h2>
+          <h2>Fitness Tracker</h2>
+          <img
+            id="team-two"
+            target="_blank"
+            rel="noreferrer"
+            src={team}
+            className="img-thumbnail img-fluid"
+            alt="fitness tracker"
+          ></img>
+          <a href="https://github.com/mbubel/fitness-tracker">
             <img
-              id="team-two"
               target="_blank"
-              src={team}
-              className="img-thumbnail img-fluid"
-              alt = "fitness tracker picture"
-            ></img>
-            <a href="https://github.com/mbubel/fitness-tracker">
-              <img
-                target="_blank"
-                src={github}
-                className="projectIcons img-fluid"
-                alt="github icon"
-              />
-            </a>
-            <a href="https://fitness-tracker-mb.herokuapp.com/">
-              <img
-                target="_blank"
-                src={heroku}
-                className="projectIcons img-fluid"
-                alt ="heroku icon"
-              />
-            </a>
+              rel="noreferrer"
+              src={github}
+              className="projectIcons img-fluid"
+              alt="github icon"
+            />
+          </a>
+          <a href="https://fitness-tracker-mb.herokuapp.com/">
+            <img
+              target="_blank"
+              rel="noreferrer"
+              src={heroku}
+              className="projectIcons img-fluid"
+              alt="heroku icon"
+            />
           </a>
         </div>
       </div>
