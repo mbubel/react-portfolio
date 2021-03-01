ba35990b5635ab7dbaf58bb0cc8ed645f6dc4d2d
Michael <michaelbubel.1989@gmail.com>
2021-03-01 14:21:33 -0800

    HashRouter attempt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+  HashRouter,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Header from "./components/Header";
@@ -12,30 +17,32 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route
-            render={({ location }) => (
-              <TransitionGroup>
-                <CSSTransition
-                  timeout={350}
-                  classNames="fade"
-                  key={location.key}
-                >
-                  <Switch location={location}>
-                    <Route path="/home" exact component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/projects" exact component={Projects} />
-                  </Switch>
-                </CSSTransition>
-              </TransitionGroup>
-            )}
-          />
+      <HashRouter basename="/">
+        <Router>
+          <div className="App">
+            <Header />
+            <Route
+              render={({ location }) => (
+                <TransitionGroup>
+                  <CSSTransition
+                    timeout={350}
+                    classNames="fade"
+                    key={location.key}
+                  >
+                    <Switch location={location}>
+                      <Route path="/home" exact component={Home} />
+                      <Route path="/about" component={About} />
+                      <Route path="/projects" exact component={Projects} />
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
+              )}
+            />
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </HashRouter>
     );
   }
 }
