8f0fca14ff42cdf2bc96ac215ffd4b136a735a72
Michael <michaelbubel.1989@gmail.com>
2021-03-02 18:17:27 -0800

    Fixed 404 issue

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import { HashRouter, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import "bootstrap/dist/css/bootstrap.css";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
-      <Router>
         <div className="App">
           <Header />
-          <Route
-            render={({ location }) => (
-              <TransitionGroup>
-                <CSSTransition
-                  timeout={350}
-                  classNames="fade"
-                  key={location.key}
-                >
-                  <Switch location={location}>
-                    <Route path="/home" exact component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/projects" exact component={Projects} />
-                  </Switch>
-                </CSSTransition>
-              </TransitionGroup>
-            )}
-          />
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/projects" exact component={Projects} />
+          </Switch>
 
           <Footer />
         </div>
-      </Router></HashRouter>
+      </HashRouter>
     );
   }
 }
